a9753df2aff767df23aee79fff675006c08021b4
Steve Brownlee <chortlehoort@gmail.com>
2020-06-03 14:09:12 -0500

    Martin's Mayan Vision code

--- scripts/fish/fishData.js
+++ scripts/fish/fishData.js
@@ -17,7 +17,7 @@ const fishCollection = [
     },
     {
         species: "Swordfish",
-        length: 320,
+        length: 323,
         location: "Atlantic Ocean",
         picture: "./images/chester.jpeg",
         food: "Herring",
@@ -41,10 +41,97 @@ const fishCollection = [
     },
     {
         species: "Orange Roughy",
-        length: 16,
+        length: 10,
+        location: "Southern Ocean",
+        picture: "./images/roughy.jpeg",
+        food: "Anchovies",
+        name: "Michael"
+    },
+    {
+        species: "Trigger Fish",
+        length: 12,
+        location: "Bay of Bengal",
+        picture: "./images/triggerfish.jpg",
+        food: "Baby squid",
+        name: "Bubbles"
+    },
+    {
+        species: "Parrot Fish",
+        length: 8,
+        location: "Sea of Google",
+        picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg",
+        food: "Popcorn",
+        name: "Angus"
+    },
+    {
+        species: "Swordfish",
+        length: 217,
+        location: "Atlantic Ocean",
+        picture: "./images/chester.jpeg",
+        food: "Herring",
+        name: "Chester"
+    },
+    {
+        species: "Angel Fish",
+        length: 8,
+        location: "Pacific Ocean",
+        picture: "./images/angelfish.jpg",
+        food: "Plankton",
+        name: "Michael"
+    },
+    {
+        species: "Elegant Corydoras",
+        length: 5,
+        location: "Caribbean Sea",
+        food: "Algae",
+        name: "Sweet",
+        picture: "./images/pink.jpg"
+    },
+    {
+        species: "Orange Roughy",
+        length: 20,
         location: "Southern Ocean",
         picture: "./images/roughy.jpeg",
         food: "Anchovies",
         name: "Michael"
     }
 ]
+
+const mostHolyFish = () => {
+    const holyFish = []
+
+    for (const fish of fishCollection) {
+        if (fish.length % 3 === 0) {
+            holyFish.push(fish)
+        }
+    }
+
+    return holyFish
+}
+
+const soldierFish = () => {
+    const soliderFishArray = []
+
+    for (const fish of fishCollection) {
+        if (fish.length % 5 === 0 && fish.length % 3 !== 0) {
+            soliderFishArray.push(fish)
+        }
+    }
+
+    return soliderFishArray
+}
+
+const fishPlebs = () => {
+    const plebs = []
+
+    for (const fish of fishCollection) {
+        if (fish.length % 5 !== 0 && fish.length % 3 !== 0) {
+            plebs.push(fish)
+        }
+    }
+
+    return plebs
+}
+
+
+
--- scripts/fish/fishList.js
+++ scripts/fish/fishList.js
@@ -2,18 +2,49 @@
 Responsible for generating a list of fish HTML
 representations, and putting in the browser
 */
-
 const fishList = () => {
-    // Iterate the collection of fish objects
-    for (const currentFishObject of fishCollection) {
+    displayTheHolyOnes()
+    displaySoldier()
+    displayPlebs()
+}
 
-        // Convert the current fish to its HTML representation
-        const fishHTML = fishConverter(currentFishObject)
+const displayTheHolyOnes = () => {
+    // Get holy fish array
+    const holyFish = mostHolyFish()
 
-        // Find the <article> element in index.html
+    // Iterate the array of holy fish
+    for (const holyFishObject of holyFish) {
+        const holyFishHTMLRepresentation = fishConverter(holyFishObject)
+
         const fishArticleElement = document.querySelector(".fishList")
-
-        // Put the fish HTML representation inside the <article> element
-        fishArticleElement.innerHTML += fishHTML
+        fishArticleElement.innerHTML += holyFishHTMLRepresentation
     }
 }
+
+const displaySoldier = () => {
+    // Get soldier fish array
+    const soldierFishArray = soldierFish()
+
+    // Iterate the array of soldier fish
+    for (const soldierFishObject of soldierFishArray) {
+        const soldierFishHTMLRepresentation = fishConverter(soldierFishObject)
+
+        const fishArticleElement = document.querySelector(".fishList")
+        fishArticleElement.innerHTML += soldierFishHTMLRepresentation
+    }
+}
+
+
+const displayPlebs = () => {
+    // Get plebs fish array
+    const plebsFishArray = fishPlebs()
+
+    // Iterate the array of plebs fish
+    for (const plebsFishObject of plebsFishArray) {
+        const plebsFishHTMLRepresentation = fishConverter(plebsFishObject)
+
+        const fishArticleElement = document.querySelector(".fishList")
+        fishArticleElement.innerHTML += plebsFishHTMLRepresentation
+    }
+}
+
